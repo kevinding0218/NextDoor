40f1bcc6b7b77776690345fb241137c39c65d9a3
kevinding0218 <kevinding0218@gmail.com>
2019-06-17 13:12:50 -0400

    creating shell script to automate solution

--- one_solution.sh
+++ one_solution.sh
@@ -1,6 +1,3 @@
-# Creae one Solution to include all projects
-- Create a shell script (one_solution.sh)
-```
 #!/bin/bash
 PROJECT_PREFIX=NextDoor
 PROJECT_PATH_PREFIX=H:\\MyGithub
@@ -42,18 +39,3 @@ do
 done
 
 $SHELL
-```
-- Run in Powershell Individually
-```
-dotnet new sln -n NextDoor
-dotnet sln NextDoor.sln add H:\MyGithub\NextDoor\NextDoor.Core\src\NextDoor.Core\NextDoor.Core.csproj
-dotnet sln NextDoor.sln add H:\MyGithub\NextDoor\NextDoor.Services.Customers\src\NextDoor.Services.Customers\NextDoor.Services.Customers.csproj
-dotnet sln NextDoor.sln add H:\MyGithub\NextDoor\NextDoor.Services.Identity\src\NextDoor.Services.Identity\NextDoor.Services.Identity.csproj
-```
-- Create a powershell script
-```
-$projects = Get-ChildItem -Recurse | Where-Object { $_.Name -match '^.+\.(csproj|vbproj)$' }
-$uniqueProjects = $projects | Group-Object -Property Name | Where Count -EQ 1 | select -ExpandProperty Group | % { $_.FullName }
-Invoke-Expression -Command "dotnet new sln -n NextDoor"
-$uniqueProjects | % { Invoke-Expression -Command "dotnet sln NextDoor.sln add ""$_""" }
-```
